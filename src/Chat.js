d1792aa4cdfa1e74e517320a0d1be2835ead7916
yawwusu <yawwusugh@gmail.com>
2021-05-03 12:50:56 +0200

    Initial template setup - app running

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.css'
+
+function Chat() {
+    return (
+        <div>
+            <h1>I am the Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBQEneHvhdA-gUVH6eOgR_zdbH1qw8BtOM",
+    authDomain: "whatsapp-mern-stack-4a60d.firebaseapp.com",
+    projectId: "whatsapp-mern-stack-4a60d",
+    storageBucket: "whatsapp-mern-stack-4a60d.appspot.com",
+    messagingSenderId: "405042936538",
+    appId: "1:405042936538:web:e4bec71a7eb31dbac343e3"
+  };
