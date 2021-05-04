d4b9e9d78c953adfe8b99a4f4f4313d542d5e108
yawwusu <yawwusugh@gmail.com>
2021-05-04 13:37:06 +0200

    Added Chat, Sidebar and SidebarChat components with styling

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,47 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Chat.css'
 
 function Chat() {
     return (
-        <div>
-            <h1>I am the Chat</h1>
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className={`chat__message ${true && "chat__receiver"}`}>
+                    <span className="chat__name">Yaw</span>
+                    This is a message
+                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">Send message</button>
+                </form>
+                <Mic />
+            </div>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,41 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 import './Sidebar.css'
+import SidebarChat from './SidebarChat'
 
 function Sidebar() {
     return (
-        <div>
-            <h1>I am a sidebar</h1>
+        <div className="sidebar">
+
+            <div className="sidebar__header">
+                <Avatar />
+
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <Chat />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>        
         </div>
     )
 }
